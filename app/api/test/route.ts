8270157624f008f73e7e41be3599eb6fe5f32550
reelmza <jessemoses71@gmail.com>
2024-12-28 12:00:12 +0100

    Initiated mongodb databse

---
+++ app/api/test/route.ts
@@ -0,0 +1,14 @@
+import prisma from "@/libs/PrismaClient";
+export const GET = async () => {
+  try {
+    const data = await prisma.trust.create({
+      data: { name: "First Nigerian Trust Limited" },
+    });
+
+    console.log(data);
+    return Response.json({ data });
+  } catch (error) {
+    console.log(error);
+    return Response.json({ msg: "Server error" });
+  }
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,23 @@
+"use client";
 import { SignOutButton } from "@clerk/nextjs";
+import prisma from "@/libs/PrismaClient";
 
 export default function Home() {
+  const testPrisma = async () => {
+    try {
+      const res = await fetch(`/api/test`);
+      if (res.ok) {
+        const data = await res.json();
+        console.log(data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <>
       <SignOutButton />
+      <button onClick={testPrisma}>Test prisma database</button>
     </>
   );
 }
